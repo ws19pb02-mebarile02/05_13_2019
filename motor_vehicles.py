cff201831bb96070e6b0a4ae41caa16272d1d13a
Michael Barile <33465350+mebarile02@users.noreply.github.com>
2019-05-14 16:03:24 -0400

    Add files via upload

---
+++ motor_vehicles.py
@@ -0,0 +1,62 @@
+import sys
+import csv   #Comma-separated values.  Do not name this Python script csv.py.
+import datetime
+import urllib.request
+import io
+import pandas as pd
+from functools import reduce
+
+url = "https://data.cityofnewyork.us/api/views/h9gi-nx95/rows.csv" \
+    "?accessType=DOWNLOAD"
+
+try:
+    fileFromUrl = urllib.request.urlopen(url)
+except urllib.error.URLError as error:
+    print("urllib.error.URLError", error)
+    sys.exit(1)
+
+sequenceOfBytes = fileFromUrl.read() #Read whole file into one big sequenceOfBytes.
+fileFromUrl.close()
+
+try:
+    s = sequenceOfBytes.decode("utf-8")    #s is a string
+except UnicodeError as unicodeError:
+    print(unicodeError)
+    sys.exit(1)
+
+fileFromString = io.StringIO(s)
+df = pd.read_csv(fileFromString) #reads in fileFromString as DataFrame
+fileFromString.close()
+
+to_drop = ['LATITUDE',          
+          'LONGITUDE',
+          'LOCATION',
+           'CONTRIBUTING FACTOR VEHICLE 2',
+           'CONTRIBUTING FACTOR VEHICLE 3',
+           'CONTRIBUTING FACTOR VEHICLE 4',
+           'CONTRIBUTING FACTOR VEHICLE 5',
+           'VEHICLE TYPE CODE 1',
+           'VEHICLE TYPE CODE 2',
+           'VEHICLE TYPE CODE 3',
+           'VEHICLE TYPE CODE 4',
+           'VEHICLE TYPE CODE 5'
+          ]
+
+
+df.drop(columns=to_drop, inplace=True) #drops listed columns
+is_11374 =  df['ZIP CODE'] == 11374  #returns list of booleans
+df_11374 = df[is_11374]  #produces dataframe containing info for 11374 zip only
+
+print('Head of edited dataframe')
+print(df_11374.head())
+print()
+print('Top 5 on street accident sites in Rego Park') 
+print(df_11374['ON STREET NAME'].value_counts().head())
+print()
+print('Top 5 cross street accident sites in Rego Park')
+print(df_11374['CROSS STREET NAME'].value_counts().head())
+print()
+print('Top 5 off street accident sites in Rego Park')
+print(df_11374['OFF STREET NAME'].value_counts().head())
+
+sys.exit(0)
